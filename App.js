af9fbed058ba53ec1918dfce042f39a3dc115e5e
sai-sahoo <mail2saisahoo@gmail.com>
2023-10-15 05:01:56 +0530

    Laying foundation

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-const parent = React.createElement('div',{id: "parent"},[
+/* const parent = React.createElement('div',{id: "parent"},[
     React.createElement('div',{id: "child1"},[
         React.createElement('h1',{},'H1 Tag'),
         React.createElement('h1',{},'H2 Tag')]
@@ -10,6 +10,19 @@ const parent = React.createElement('div',{id: "parent"},[
         React.createElement('h1',{},'H2 Tag')]
     )
     ]
+); */
+const elem = <div>React JSX ELEM</div>;
+const title = (
+    <h1 id="title" className="title" tabIndex="5">
+        {elem}
+        Title Component
+    </h1>
+);
+const HeadingComponent = () => (
+    <div id="container">
+        {title}
+        <h1 id="heading" className="heading" tabIndex="5">Heading Component</h1>
+    </div>
 );
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(parent);
+root.render(<HeadingComponent />);
