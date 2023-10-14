f37c12f5c2a6439ec186d08a4e76c276c425232e
sai-sahoo <mail2saisahoo@gmail.com>
2023-10-14 15:12:46 +0530

    Ep-02

--- App.js
+++ App.js
@@ -1,11 +1,5 @@
-/*
-<div id="parent">
-    <div id="child">
-        <h1>siblings</h1>
-        <h2>siblings</h2>
-    </div>
-</div>
-*/
+import React from "react";
+import ReactDOM from "react-dom/client";
 const parent = React.createElement('div',{id: "parent"},[
     React.createElement('div',{id: "child1"},[
         React.createElement('h1',{},'H1 Tag'),
@@ -17,6 +11,5 @@ const parent = React.createElement('div',{id: "parent"},[
     )
     ]
 );
-console.log(parent);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(parent);
